0d8ff565ebc289a0ec68a0a5501ddd4735c0305e
yi <igim33190@gmail.com>
2025-10-14 17:46:08 +0900

    250928

--- 250928/backjun_storage.py
+++ 250928/backjun_storage.py
@@ -2,7 +2,8 @@ import sys
 sys.stdin = open('input.txt')
 
 T = int(input())
-arr = [0] * 1000
+
+arr = []
 
 max_l = 0
 min_l = 1000
@@ -10,15 +11,39 @@ min_l = 1000
 max_h = 0
 start = 0
 
+max_index = 0
+
 for _ in range(T):
     l, h = map(int, input().split())
 
-    arr[l] = h
+    arr.append((h, l))
+
+    # 시작점
     min_l = min(l, min_l)
+    # 끝점
     max_l = max(l, max_l)
+    # 최대 높이
     max_h = max(h, max_h)
 
-for i in range(1000):
+arr = sorted(arr)
+
+while arr:
+    h, l = arr.pop()
+
+
+        
+            
+
+
+
+
+
+
+
+
+
+
+
     
 
 
