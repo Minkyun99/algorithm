9956a8f517f95da912bd06e0571e9bbd07ee9d18
yi <igim33190@gmail.com>
2025-10-21 18:01:21 +0900

    251021

--- 250928/backjun_storage.py
+++ 250928/backjun_storage.py
@@ -1,9 +1,7 @@
-# import sys
-# sys.stdin = open('input.txt')
+import sys
+sys.stdin = open('input.txt')
 
 T = int(input())
-
-arr = []
 
 max_l = 0
 min_l = 1000
@@ -12,76 +10,77 @@ max_h = 0
 start = 0
 
 max_index = 0
+arr = [0] * 1001
 
 for _ in range(T):
     l, h = map(int, input().split())
-
-    arr.append((h, l))
-
     # 시작점
     min_l = min(l, min_l)
+
     # 끝점
     max_l = max(l, max_l)
-
-
-sort_arr = sorted(arr)
-print(sort_arr)
-
-max_idx = sort_arr[-1][0]
-max_h = sort_arr[-1][1]
-total_square = (max_l - min_l) * max_h
+    arr[l] = h
 
 left_arr = []
 right_arr = []
 
-for h, l in sort_arr:
-    if l < max_idx:
-        left_arr.append((l, h))
-    else :
-        right_arr.append((l, h))
+max_value = max(arr)
+max_index = arr.index(max_value)
 
-left_arr = sorted(left_arr)
-right_arr = sorted(right_arr)
+total_square = ((max_l+1) - min_l) * max_value
 
-max_len = left_arr[0][1]
-idx = l
-left_max_h = max_h
-left_max_i = max_idx
-while left_max_i >= min_l:
-    if left_max_i < min_l:
+for i in range(min_l, max_l+1):
+    if i < max_index:
+        left_arr.append(arr[i])
+    else:
+        right_arr.append(arr[i])
+
+
+# 가장 높은 기둥을 기준으로 왼쪽
+result_left_idx = max_index - min_l
+
+while len(left_arr) > 0:
+    max_left = max(left_arr)
+    max_left_index = left_arr.index(max_left)
+
+    if max_left_index == 0:
+        small_sqaure = (max_value - max_left) * result_left_idx
+        total_square = total_square - small_sqaure
+        left_arr = []
         break
-    for i in range(len(left_arr)-1):
-        if max_len <= left_arr[i][0] :
-            max_len = left_arr[i][1]
-            idx = left_arr[i][0]
-            remove_item = i
-    left_arr.pop(remove_item)
-    small_sqaure = (left_max_h - max_len) * (left_max_i - idx)
 
-    for j in range(len(left_arr)-1):
-        if left_arr[j][0] > idx:
-            left_arr.pop(j)
-
-    left_max_h = max_len
-    left_max_i = idx
-
-
+    left_arr = left_arr[0:max_left_index]
     
+    small_sqaure = (max_value - max_left) * (result_left_idx - max_left_index)
+    total_square = total_square - small_sqaure
+    result_left_idx = max_left_index
 
 
+# 가장 높은 기둥을 기준으로 오른쪽
+result_right_idx = (max_l+1) - (max_index+1)
+
+right_arr.pop(0)
+
+while len(right_arr) > 0:
+    max_right = max(right_arr)
+    max_right_index = right_arr.index(max_right)
+
+    if max_right_index == len(right_arr)-1:
+        small_sqaure = (max_value - max_right) * result_right_idx
+        total_square = total_square - small_sqaure
+        right_arr = []
+        break
+
+
+    if max_right == max_value:
+        right_arr = right_arr[max_right_index+1:len(right_arr)]
+        result_right_idx = len(right_arr)
+    
+    else:
+        right_arr = right_arr[max_right_index+1:len(right_arr)]   
+        small_sqaure = (max_value - max_right) * (result_right_idx - max_right_index)
+        total_square = total_square - small_sqaure
+        result_right_idx = max_right_index
         
-            
 
-
-
-
-
-
-
-
-
-
-
-    
-
-
+print(total_square)
